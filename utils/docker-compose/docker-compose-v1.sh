c84e9f792c71f53a26191a8ba6ac2f5f8a5891a3
Anthony Guerreiro <anthony@aguerreiro.com>
2018-11-02 00:24:55 +0000

    Polish

--- utils/docker-compose/docker-compose-v1.sh
+++ utils/docker-compose/docker-compose-v1.sh
@@ -7,7 +7,6 @@ source .env
 
 cp docker-compose.yml.v1 tmp.yml
 
-sed -i -e "s/version/asd/g" tmp.yml
 sed -i -e "s/\${trakt_user}/$trakt_user/g" tmp.yml
 sed -i -e "s/\${mal_user}/$mal_user/g" tmp.yml
 sed -i -e "s/\${pushbullet_token}/$pushbullet_token/g" tmp.yml
